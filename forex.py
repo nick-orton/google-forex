731c8a0142ccb6d0f5aeac079c62e80475203a14
no <no@frog.(none)>
2009-08-02 02:57:02 -0400

    made executable and supress deprecation warning from parser

--- forex.py
+++ forex.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+import warnings
 import urllib
 import html5lib
 from html5lib.treebuilders import dom
@@ -10,20 +12,31 @@ if (len(sys.argv) != 3):
 currency1 = sys.argv[1] 
 currency2 = sys.argv[2]
 
+def url_for(currency1, currency2):
+        """builds a get request for google's currency converter from the two currencie"""
+        pass
 url = "http://www.google.com/finance/converter?a=1&from=" + currency1 + "&to=" + currency2
 
 response = urllib.urlopen(url)
 data = response.read()
 
-# Create an HTML parser which creates DOMs.
-parser = html5lib.HTMLParser(tree=dom.TreeBuilder)
-# Parse the source.
-tree = parser.parse(data)
-# Normalise the tree.  This basically cleans up the
-# text nodes inside which makes our life easier.
-tree.normalize()
+def get_tree_from(html):
+    """builds a dom from a html string"""
+    # Create an HTML parser which creates DOMs.
+    parser = html5lib.HTMLParser(tree=dom.TreeBuilder)
+    # Parse the source.
+    tree = parser.parse(html)
+    # Normalise the tree.  This basically cleans up the
+    # text nodes inside which makes our life easier.
+    tree.normalize()
+    return tree
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    tree = get_tree_from(data)
 
 # Create a list of all the rows (that is, all <div>
+
 # tags with the class of "row")
 rows = [div for div in tree.getElementsByTagName("div")\
         if div.getAttribute("id").strip() == "currency_converter_result"]
