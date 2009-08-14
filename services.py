a4d4f05ea4fd908f8af990a2bdd478527da411f6
no <no@frog.(none)>
2009-08-13 20:12:19 -0400

    making the scrape nicer

--- services.py
+++ services.py
@@ -24,9 +24,8 @@ class GoogleForexRateClient():
     def _scrape_url_for_response(self,tree):
         """ Pulls the desired data out of the dom """
         # TODO nicer scrape [#8]
-        meaty_bits = [div for div in tree.getElementsByTagName("div")\
-            if div.getAttribute("id").strip() == "currency_converter_result"][0]
-        quote = meaty_bits.childNodes[1].childNodes[0].data
+        quote = [span for span in tree.getElementsByTagName("span")\
+            if span.getAttribute("class").strip() == "bld"][0].childNodes[0].data
         amount = quote.split()[0]
         return amount
 
